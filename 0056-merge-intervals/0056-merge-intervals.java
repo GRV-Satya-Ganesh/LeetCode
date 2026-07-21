bec1bfc50051027b5e947ed17274b84ee4729a6d
GRV-Satya-Ganesh <grvsatyaganesh@gmail.com>
2026-07-21 09:41:52 +0530

    0056-MERGE-INTERVALS Stats: Time: 10 ms (22.1%), Space: 49 MB (75.18%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -4,34 +4,28 @@ class Solution {
         int n = intervals.length;
 
         List<List<Integer>> result = new ArrayList<>();
-        int visited[] = new int[n];
 
         Arrays.sort(intervals, (a,b) -> (a[0] - b[0]));
 
-        for(int i = 0; i < n; i++){
-            if(visited[i] == 1){
-                continue;
-            } 
-
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            int j = i+1;
-            while(j < n && intervals[j][0] <= end){
-                end = Math.max(end, intervals[j][1]);
-                visited[j] = 1;
-                j++;
+        int i = 0;
+        while(i < n){
+            if(result.isEmpty() || result.get(result.size() - 1).get(1) < intervals[i][0]){
+                result.add(Arrays.asList(intervals[i][0], intervals[i][1]));
             }
-            
-            result.add(List.of(start, end));
-            visited[i] = 1;
+            else{
+                int end1 = result.get(result.size() - 1).get(1);
+                end1 = Math.max(end1, intervals[i][1]);
+
+                result.get(result.size() - 1).set(1, end1);
+            }
+            i++;
         }
 
 
         int r = result.size();
 
         int[][] ans = new int[r][2];
-        for(int i = 0; i < r; i++){
+        for(i = 0; i < r; i++){
             ans[i][0] = result.get(i).get(0);
             ans[i][1] = result.get(i).get(1);
         }
