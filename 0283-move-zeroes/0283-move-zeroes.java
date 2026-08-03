63a7cf41d21231ad057e131b8f78e1d2ef37f4c4
GRV-Satya-Ganesh <grvsatyaganesh@gmail.com>
2026-08-03 08:53:21 +0530

    0283-MOVE-ZEROES Stats: Time: 30 ms (6.23%), Space: 47.8 MB (57.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,11 +2,10 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
         int l = 0; 
-        int r = l+1;
 
-        while(l < r){
+        while(l < n){
             while(l < n && nums[l] != 0) l++;
-            r = l + 1;
+            int r = l + 1;
             while(r < n && nums[r] == 0) r++;
             
             if(r >= n) break;
